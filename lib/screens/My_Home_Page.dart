b1128a80c65b71bc7786eec01ff23f20556b0e24
JaskaranSingh-13 <jaskarn.singh582003@gmail.com>
2024-04-12 15:18:15 -0600

    My First Flutter Commit

---
+++ lib/screens/My_Home_Page.dart
@@ -0,0 +1,91 @@
+// ignore_for_file: depend_on_referenced_packages, file_names
+
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController nameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 8, 136, 158),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+              height: 70,
+              child: Image.asset('assets/rdplogo.png'),
+            ),
+            const Text(
+              'Daily Planner',
+              style: TextStyle(
+                fontFamily: 'Caveat',
+                fontSize: 32,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        color: Colors.grey[200],
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                height: 300,
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: TableCalendar(
+                    calendarFormat: CalendarFormat.month,
+                    headerVisible: true,
+                    focusedDay: DateTime.now(),
+                    firstDay: DateTime(2023),
+                    lastDay: DateTime(2025),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 100,
+                    width: 350,
+                    child: TextField(
+                      maxLength: 20,
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        labelText: 'Add To-Do List Item',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: null,
+                child: Text('Submit'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
